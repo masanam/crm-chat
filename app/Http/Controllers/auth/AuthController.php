3ee81573000f449e46a4cd790f9291f2fb852788
Choirul Anam <masanam@yahoo.com>
2024-06-13 20:33:22 +0700

    update internal

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
     if (Auth::attempt($credentials)) {
       $request->session()->regenerate();
 
-      return redirect('dashboard');
+      return redirect('/');
     }
     // } else {
     //     if (Auth::attempt($credentials)) {
